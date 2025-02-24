4bb232654c6345378861f1eb53a4775460559a51
Alexandra Kiseleva <sahsakis2512@gmail.com>
2025-02-24 18:58:59 +0400

    Styled project

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import './App.css';
 import { Header } from './components/Header/Header.jsx';
-import { useState } from 'react';
 import Filters from './components/Filters/Filters.jsx';
 import Tabs from './components/Tabs/Tabs.jsx';
 import Card from './components/Card/Card.jsx';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header />
-      <Filters />
-      <Tabs />
-      <Card />
+      <div className="wrapper">
+        <Filters />
+        <section className="content">
+          <Tabs />
+          <Card />
+        </section>
+      </div>
     </>
   );
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -8,25 +8,33 @@ export default function Card() {
           <span>13 400 P</span>
           <img src="/assets/S7%20Logo.svg" alt="logo" />
         </div>
-        <div className={cardStyle.description}>
-          <span>Mow-Hkt</span>
-          <span>В пути</span>
-          <span>2 пересадки</span>
+        <div className={cardStyle.infoWrapper}>
+          <div className={cardStyle.from}>
+            <span className={cardStyle.grayLine}>Mow - Hkt</span>
+            <span>10:45 - 8:00</span>
+          </div>
+          <div className={cardStyle.time}>
+            <span className={cardStyle.grayLine}>В пути</span>
+            <span>21ч 15м</span>
+          </div>
+          <div className={cardStyle.transfer}>
+            <span className={cardStyle.grayLine}>2 пересадки</span>
+            <span>HKG, JNB</span>
+          </div>
         </div>
-        <div className={cardStyle.time}>
-          <span>10:45-8:00</span>
-          <span>21ч 15м</span>
-          <span>HKG,JNB</span>
-        </div>
-        <div className={cardStyle.description}>
-          <span>Mow-Hkt</span>
-          <span>В пути</span>
-          <span>1 пересадка</span>
-        </div>
-        <div className={cardStyle.time}>
-          <span>11:20-00:50</span>
-          <span>13ч 30м</span>
-          <span>HKG</span>
+        <div className={cardStyle.infoWrapper}>
+          <div className={cardStyle.from}>
+            <span className={cardStyle.grayLine}>Mow - Hkt</span>
+            <span>11:20 - 00:50</span>
+          </div>
+          <div className={cardStyle.time}>
+            <span className={cardStyle.grayLine}>В пути</span>
+            <span>13ч 30м</span>
+          </div>
+          <div className={cardStyle.transfer}>
+            <span className={cardStyle.grayLine}>1 пересадка</span>
+            <span>HKG</span>
+          </div>
         </div>
       </div>
     </>
